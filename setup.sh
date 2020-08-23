c0646613c1a399623b3d2712145166afe9378bb2
Calum Daines <calum-daines@live.com>
2020-08-23 16:13:52 +0100

    feat: script to modify basic values for install

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "[?] Value for namespace: "
+read varnamespace
+
+kubectl create ns $varnamespace
+
+echo "[?] Value for datacenter: "
+read vardatacenter
+
+echo '[+] Modifying setup files with new values'
+
+mod_files="ambassador-consul-connector-tls.yaml
+           ambassador-rbac-tls.yaml
+           consul-resolver.yaml
+           consul-values.yaml"
+
+for file in $mod_files
+    do
+        sed -i '' -e 's/{namespace}/'$varnamespace'/g' $file
+        sed -i '' -e 's/{datacenter}/'$vardatacenter'/g' $file
+    done
+
+echo '[+] Done'
